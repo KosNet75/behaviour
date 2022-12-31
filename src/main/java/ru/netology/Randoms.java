7f0dcdc655d91688add6bc2219c747f9257ea68e
KosNet75 <nemokonstantin@gmail.com>
2022-12-31 15:53:45 +0700

    Final commit

---
+++ src/main/java/ru/netology/Randoms.java
@@ -0,0 +1,36 @@
+package ru.netology;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random = new Random();
+    private final int min;
+    private final int max;
+
+    protected Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+        if (min >= max) {
+            throw new IllegalArgumentException("Число 'min' должно быть меньше числа 'max'!");
+        }
+        random.nextInt(min, max);
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new RandomsIterator();
+    }
+
+    private class RandomsIterator implements Iterator<Integer> {
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+
+        @Override
+        public Integer next() {
+            return random.nextInt(min, max + 1);
+        }
+    }
+}
